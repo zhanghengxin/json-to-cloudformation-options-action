ef4e9462705e130f298bca9b23b35cf2d935123c
不需要知道我来自何方--myStar <812718649@qq.com>
2022-10-20 13:56:51 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,17 +4,19 @@ const core = require('@actions/core');
 (async () => {
     try {
         const path = core.getInput('path');
+        const property = core.getInput('property');
         const data = await fs.promises.readFile(path);
         const json = JSON.parse(data);
         let toReturn = ''
 
-        for(const property in json) {
+        for(const prop in json) {
             if(toReturn.length > 0) {
                 toReturn += ' ';
             }
-            toReturn += `"${property}=${json[property]}"`
+            toReturn += `"${prop}=${json[prop]}"`
         }
         core.setOutput("value", toReturn);
+        core.setOutput("property", json[property]
     } catch (error) {
    		core.setFailed(error.message);
     }
